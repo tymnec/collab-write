23a6fb010d20db634a6d6e9f8e8dc21b388975c1
Nikhil <tymnec@gmail.com>
2023-12-22 00:25:19 +0530

    dynamic next imports client side

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,45 +1,18 @@
 "use client";
 
-import React, { useCallback, useRef } from "react";
+import React from "react";
+import { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+import dynamic from "next/dynamic";
 
-// Import Quill necessary modules
-import Quill from "quill";
-import "quill/dist/quill.snow.css";
-
-const SAVE_INTERVAL_MS = 2000;
-const TOOLBAR_OPTIONS = [
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  [{ font: [] }],
-  [{ list: "ordered" }, { list: "bullet" }],
-  ["bold", "italic", "underline"],
-  [{ color: [] }, { background: [] }],
-  [{ script: "sub" }, { script: "super" }],
-  [{ align: [] }],
-  ["image", "blockquote", "code-block"],
-  ["clean"],
-];
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const Editor = () => {
-  const [quill, setQuill] = React.useState<Quill | null>(null);
-
-  const wrapperRef = useCallback((wrapper: any) => {
-    if (wrapper == null) return;
-
-    wrapper.innerHTML = "";
-    const editor = document.createElement("div");
-    wrapper.append(editor);
-    const q = new Quill(editor, {
-      theme: "snow",
-      modules: { toolbar: TOOLBAR_OPTIONS },
-    });
-    // q.disable();
-    // q.setText("Loading...");
-    setQuill(q);
-  }, []);
+  const [value, setValue] = useState("");
 
   return (
     <div>
-      <div ref={wrapperRef} className="container shadow p-2 rounded"></div>
+      <ReactQuill value={value} onChange={setValue} theme="snow"></ReactQuill>
     </div>
   );
 };
